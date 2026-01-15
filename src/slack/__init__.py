b6e546478b3b99d5a7974761edece5b2f9a48ea9
Boris Tsekinovsky <t.boris@gmail.com>
2026-01-14 20:58:24 -0600

    feat(11-01): export history functions from slack package
    - Export fetch_channel_history, fetch_thread_history
    - Export format_messages_for_context, ConversationContext
    - Add to __all__ for explicit public API

--- src/slack/__init__.py
+++ src/slack/__init__.py
@@ -1,6 +1,21 @@
 """Slack integration module."""
 
 from src.slack.app import get_slack_app, start_socket_mode, stop_socket_mode
+from src.slack.history import (
+    ConversationContext,
+    fetch_channel_history,
+    fetch_thread_history,
+    format_messages_for_context,
+)
 from src.slack.router import register_handlers
 
-__all__ = ["get_slack_app", "start_socket_mode", "stop_socket_mode", "register_handlers"]
+__all__ = [
+    "get_slack_app",
+    "start_socket_mode",
+    "stop_socket_mode",
+    "register_handlers",
+    "fetch_channel_history",
+    "fetch_thread_history",
+    "format_messages_for_context",
+    "ConversationContext",
+]
