fcf3181e80fbc3ba3fbde8861ea1423d43c2b02c
Boris Tsekinovsky <t.boris@gmail.com>
2026-02-02 16:56:08 -0600

    feat(02-02): update slack module exports
    - Export SlackClient and RateLimiter from client module
    - Export SlackMessage, SlackWriteTarget, WRITE_TARGETS from types module
    - Update docstring to describe module capabilities
    
    All public interfaces now available from src.slack

--- src/slack/__init__.py
+++ src/slack/__init__.py
@@ -1,5 +1,20 @@
-"""Slack integration layer."""
+"""Slack integration layer.
 
+Provides:
+- AsyncApp: Bolt app for receiving Slack events
+- SlackClient: Wrapper for sending messages with routing and rate limiting
+- SlackMessage, SlackWriteTarget: Types for Slack communication
+"""
 from src.slack.app import create_bolt_app, get_bolt_app
+from src.slack.client import SlackClient, RateLimiter
+from src.slack.types import SlackMessage, SlackWriteTarget, WRITE_TARGETS
 
-__all__ = ["create_bolt_app", "get_bolt_app"]
+__all__ = [
+    "create_bolt_app",
+    "get_bolt_app",
+    "SlackClient",
+    "RateLimiter",
+    "SlackMessage",
+    "SlackWriteTarget",
+    "WRITE_TARGETS",
+]
